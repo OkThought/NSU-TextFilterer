db8c37ea380b2d2b1409a462156b31033fce7ab3
RoyPy <bogush.vano@gmail.com>
2017-11-30 11:41:23 +0700

    Add filter_symbols function and use it in filter_file filter_symbols(txt, syms) copies txt without symbols from syms and returns it

--- src/pdf2txt/util/text_filter.py
+++ src/pdf2txt/util/text_filter.py
@@ -2,6 +2,8 @@ import re
 import shutil
 import tempfile
 
+from collections import Iterable
+
 formula_regexp_str = r'(?:\s*[()\w\d, ]*\s*[\^\-+=<>≤≥*\/|])+\s*[,()\w\d]*'
 # formula_sentence_regexp_str = r'(?:^|(?<=[.!?\n\r]).*)(?:([\'\"]).*\1)*' + formula_regexp_str + r'[^.!?\n\r]*\s*[!?.]?'
 
@@ -17,18 +19,30 @@ def contains_formula(text: str):
     return formula_regexp.search(text) is not None
 
 
+def find_formula(text: str):
+    match = formula_regexp.search(text)
+    return match.pos, match.endpos
+
+
 def is_formula_sentence(text: str):
     # return formula_sentence_regexp.fullmatch(text) is not None
     return False
 
 
-def remove_formulas_from_file(filename: str):
+def filter_symbols(text: str, symbols: Iterable):
+    return ''.join(char for char in text if char not in symbols)
+
+
+def filter_file(filename: str, filter_formulas=True, symbols_to_filter=''):
     with tempfile.TemporaryFile() as tmp_fp:
         with open(filename, 'w+') as f:
             shutil.copyfileobj(f, tmp_fp)
             f.seek(0)
             f.truncate()
             for line in tmp_fp:
-                if not contains_formula(line):
-                    f.write(line)
+                if filter_formulas and contains_formula(line):
+                    continue
+                if symbols_to_filter:
+                    filter_symbols(line, symbols_to_filter)
+
 
