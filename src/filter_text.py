58702ae97015fadfb7b4e59477913ef762ab27e6
RoyPy <bogush.vano@gmail.com>
2018-01-22 18:17:43 +0700

    Fix bug: when size of a src file is zero the dst file is not created

--- src/filter_text.py
+++ src/filter_text.py
@@ -9,18 +9,24 @@ from util.filter_text import TextFilterer
 def main(args=None):
     import argparse
     p = argparse.ArgumentParser()
+
     p.add_argument('src', nargs='?', default=None,
                    help='file to filter. By default `stdin`')
+
     p.add_argument('dst', nargs='?', default=None,
                    help='filtered file. By default `stdout` or if src defined')
-    p.add_argument('--keep-sentences-with-formulas', default=False, action='store_true',
-                   help='when specified does not filter sentences with formulas')
+
     default_sentence_delimiters = '.!?'
     p.add_argument('--sentence-delimiters', default=default_sentence_delimiters, nargs='*', metavar='char',
                    help='characters indicating sentence endings and beginnings')
-    default_skip_chars = string.punctuation.translate({ord(c): None for c in default_sentence_delimiters + ','})
+
+    p.add_argument('--keep-sentences-with-formulas', default=False, action='store_true',
+                   help='when specified does not filter sentences with formulas')
+
+    default_skip_chars = r'#@$\\`'
     p.add_argument('--skip-chars', default=default_skip_chars, nargs='*', metavar='char',
                    help='characters not written to dst')
+
     a = p.parse_args(args)
 
     if a.dst and a.dst == a.src or a.src and not a.dst:
@@ -31,6 +37,9 @@ def main(args=None):
 
     if os.stat(a.src).st_size == 0:
         # src file is empty
+        # create empty dst file
+        if a.dst:
+            open(a.dst, 'w').close()
         return
 
     with    open(a.src, 'r') if a.src else sys.stdin as src, \
@@ -38,9 +47,9 @@ def main(args=None):
         file_filterer = TextFilterer(
             src=src,
             dst=dst,
+            sentence_delimiters=a.sentence_delimiters,
             remove_sentences_with_formulas=not a.keep_sentences_with_formulas,
-            chars_to_skip=a.skip_chars,
-            sentence_delimiters=a.sentence_delimiters)
+            chars_to_remove=a.skip_chars)
         file_filterer.filter()
 
 
